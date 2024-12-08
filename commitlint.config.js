3cfb5612e6d9503ad9d5a01055140a3caf7b9a45
Tommaso <tommaso.meledina@it.ibm.com>
2024-12-08 22:53:10 +0100

    chore: more Prettier/ESlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional', 'plugin:prettier/recommended']
+  extends: ['@commitlint/config-conventional']
 };
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
 
 
 /** @type {import('eslint').Linter.Config[]} */
@@ -9,4 +10,5 @@ export default [
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  eslintPluginPrettierRecommended
 ];
