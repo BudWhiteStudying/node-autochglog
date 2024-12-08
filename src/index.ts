65804b3f0a5f1ad4f27cd79ef07a5f6b47da2478
Tommaso <tommaso.meledina@it.ibm.com>
2024-12-08 22:53:41 +0100

    refactor: apply prettier

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,19 @@
-import { getFullGitLog } from "./git-parser";
-import { FALLBACK_ERROR_MESSAGE } from "./messages";
+import { getFullGitLog } from './git-parser';
+import { FALLBACK_ERROR_MESSAGE } from './messages';
 
 const main = async () => {
+  try {
+    const gitLogInfo = await getFullGitLog('develop');
+    console.log(JSON.stringify(gitLogInfo, null, 4));
+  } catch (error: any) {
+    console.warn('caught..?');
     try {
-        const gitLogInfo = await getFullGitLog('develop');
-        console.log(JSON.stringify(gitLogInfo, null, 4));
-
-
-
-
-        
+      const parsedError = error as { message: string };
+      console.warn(parsedError.message || FALLBACK_ERROR_MESSAGE);
+    } catch (parsingError: any) {
+      console.error(JSON.stringify(error) || FALLBACK_ERROR_MESSAGE);
     }
-    catch(error : any) {
-        console.warn('caught..?')
-        try {
-            const parsedError = error as {message : string};
-            console.warn(parsedError.message || FALLBACK_ERROR_MESSAGE);
-        }
-        catch(parsingError : any) {
-            console.error(JSON.stringify(error) || FALLBACK_ERROR_MESSAGE);
-        }
-    }
+  }
 };
 
 main();
