c53e6b1ee0a9ba25f43b71e09533af7501b1c28e
Tommaso <tommaso.meledina@it.ibm.com>
2024-12-25 21:30:31 +0100

    chore: remove superfluous singleton pattern

--- src/config/configService.ts
+++ src/config/configService.ts
@@ -1,21 +1,11 @@
 import { join } from 'path';
 import { NodeAutoChglogConfig } from './NodeAutochglogConfig';
 
-let config: NodeAutoChglogConfig | null = null;
-
-const getConfig: () => NodeAutoChglogConfig = () => {
-  if (config) {
-    return config;
-  } else {
-    // check for custom config
-    // merge it with the default config, if necessary
-    // return the config
-    return defaultConfig;
-  }
-};
-
 export const getRuntimeConfig: () => NodeAutoChglogConfig = () => {
-  return getConfig();
+  // check for custom config
+  // merge it with the default config, if necessary
+  // return the config
+  return defaultConfig;
 };
 
 const defaultConfig: NodeAutoChglogConfig = {
