6b6a895e71ea805eb89a8705d0cbb63f28f154d9
Tommaso <tommaso.meledina@it.ibm.com>
2024-12-25 21:52:38 +0100

    feat: support custom config

--- src/config/configService.ts
+++ src/config/configService.ts
@@ -1,11 +1,39 @@
 import { join } from 'path';
-import { NodeAutoChglogConfig } from './NodeAutochglogConfig';
+import {
+  CustomNodeAutoChglogConfig,
+  NodeAutoChglogConfig
+} from './NodeAutochglogConfig';
+import { readFileSync } from 'fs';
+
+const getCustomConfig: () => CustomNodeAutoChglogConfig | null = () => {
+  try {
+    return JSON.parse(
+      readFileSync(join(process.cwd(), 'node-autochglog.config.json'), 'utf-8')
+    );
+  } catch (e) {
+    console.info(`Custom config was not loaded (${e})`);
+    return null;
+  }
+};
+
+const processConfig: (
+  inputConfig: NodeAutoChglogConfig
+) => NodeAutoChglogConfig = (inputConfig: NodeAutoChglogConfig) => {
+  return {
+    ...inputConfig,
+    outputFilepath: join(process.cwd(), inputConfig.outputFilepath),
+    templateLocation: inputConfig.templateLocation
+      ? join(process.cwd(), inputConfig.templateLocation)
+      : join(__dirname, 'DEFAULT_TEMPLATE.mustache')
+  };
+};
 
 export const getRuntimeConfig: () => NodeAutoChglogConfig = () => {
-  // check for custom config
-  // merge it with the default config, if necessary
-  // return the config
-  return defaultConfig;
+  const customConfig = getCustomConfig();
+  return processConfig({
+    ...defaultConfig,
+    ...customConfig
+  });
 };
 
 const defaultConfig: NodeAutoChglogConfig = {
@@ -14,7 +42,7 @@ const defaultConfig: NodeAutoChglogConfig = {
   initialTag: 'Unreleased',
   templateLocation: join(__dirname, 'DEFAULT_TEMPLATE.mustache'),
   targetBranch: 'develop',
-  outputFilepath: join(process.cwd(), 'CHANGELOG.md'),
+  outputFilepath: join('CHANGELOG.md'),
   allowedCategories: [
     {
       key: 'feat',
